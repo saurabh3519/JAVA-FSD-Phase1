4ea3669fbfa2d5ebdde15ad0697d7c2451596dea
saurabh3519 <saurabhsingh3519@gmail.com>
2022-08-12 13:35:38 +0530

    napropject3 commit

---
+++ Demo/AssistedPropject/AssistedProjectPhase-2/NAproject3/src/main/java/com/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.service;
+
+import com.bean.Product;
+import com.dao.ProductDao;
+
+public class ProductService {
+ProductDao pd = new ProductDao();
+	
+	public String storeProduct(Product product) {
+		if(product.getPrice()<100) {
+			return "Product price must be > 100";
+		}else if(pd.storeProduct(product)>0) {
+			return "Product stored successfully";
+		}else {
+			return "Product didn't store id must be unique";
+		}
+	}
+
+}
